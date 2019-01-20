66aefe90956eb22689b9c6071207d3fd0843c975
VladislavG123 <gvo_step2018@mail.ru>
2019-01-20 21:22:44 +0600

    First commit

---
+++ Indexer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerHomeWork
+{
+    class Indexer
+    {
+        int _size;
+        int[] _data;
+        public Indexer(int arraySize)
+        {
+            _size = arraySize;
+            _data = new int[_size];
+        }
+        public int this[int index]
+        {
+            get { return _data[index]; }
+            set { _data[index] = value * 2; }
+        }
+    }
+}
